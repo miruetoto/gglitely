cfc04af641f056ef3d4935b5cd1d822dacf09e54
miruetoto <miruetoto@gmail.com>
2024-01-19 13:34:59 +0900

    .

--- gglitely/allcodes.py
+++ gglitely/allcodes.py
@@ -8,6 +8,28 @@ import numpy as np
 
 #---#
 
+COL_MAPPING = {
+    1: 'black',
+    2: 'red',
+    3: 'green',
+    4: 'blue',
+    5: 'purple',
+    6: 'cyan',
+    7: 'orange',
+    8: 'yellow'
+}        
+
+LTY_MAPPING = {
+    0: None,                # 'blank' - no line
+    1: 'solid',             # 'solid' - solid line
+    2: '8px,3px,8px,3px',   # 'dashed' - dashed line
+    3: 'dot',               # 'dotted' - dotted line
+    4: 'dashdot',           # 'dotdash' - dash-dot line
+    5: 'longdash',          # 'longdash' - long dashed line
+    6: 'longdashdot',       # 'twodash' - two dash line (not directly equivalent in Plotly, but 'longdashdot' is close)
+}
+#---#
+
 class gglitely(Figure):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -25,44 +47,13 @@ class gglitely(Figure):
 
 class point(go.Scatter):
     def __init__(self, *args, **kwargs):       
-        color = kwargs.pop('colour', kwargs.pop('col', kwargs.pop('color', None)))
-
-        symbol = kwargs.pop('symbol',kwargs.pop('pch', kwargs.pop('shape', 'circle')))
-        pch_mapping = {
-            0: 'square-open',
-            1: 'circle-open',
-            2: 'triangle-up-open',
-            3: 'cross-open',
-            4: 'x-open',
-            5: 'diamond-open',
-            6: 'triangle-down-open',
-            7: 'cross',
-            8: 'x',
-            9: 'diamond-tall-open',
-            10: 'circle',
-            11: 'diamond-tall',
-            12: 'square',
-            13: 'cross-thin-open',
-            14: 'diamond-open',
-            15: 'square',
-            16: 'circle',
-            17: 'triangle-up',
-            18: 'triangle-down',
-            19: 'circle',
-            20: 'circle',
-            21: 'circle',
-            22: 'square',
-            23: 'diamond-tall',
-            24: 'triangle-up',
-            25: 'triangle-down',
-        }
-        if isinstance(symbol,int):
-            symbol = pch_mapping[symbol]
-
         opacity = kwargs.pop('opacity', kwargs.pop('alpha', 1))
         if isinstance(opacity, list) or (isinstance(opacity, np.ndarray) and opacity.ndim == 1):
             opacity = np.array(opacity) / np.array(opacity).max()
-            
+        color = kwargs.pop('colour', kwargs.pop('col', kwargs.pop('color', None)))
+        if isinstance(color,int):
+            color = COL_MAPPING[color]
+        symbol = kwargs.pop('symbol',kwargs.pop('pch', kwargs.pop('shape', 'circle')))
         size = kwargs.pop('cex', kwargs.pop('size', None))            
         fillcolor = kwargs.pop('fill', None)             
         line = kwargs.pop('stroke', None)
@@ -79,21 +70,16 @@ class point(go.Scatter):
         super().__init__(mode='markers', *args, **kwargs)
 class line(go.Scatter):
     def __init__(self, *args, **kwargs):
+        opacity = kwargs.pop('opacity', kwargs.pop('alpha', 1))
+        if isinstance(opacity, list) or (isinstance(opacity, np.ndarray) and opacity.ndim == 1):
+            opacity = np.array(opacity) / np.array(opacity).max()        
         color = kwargs.pop('colour', kwargs.pop('col', kwargs.pop('color', None)))
-        
-        dash = kwargs.pop('lty', kwargs.pop('linetype', kwargs.pop('dash', None)))
-        lty_mapping = {
-            0: None,                # 'blank' - no line
-            1: 'solid',             # 'solid' - solid line
-            2: '8px,3px,8px,3px',   # 'dashed' - dashed line
-            3: 'dot',               # 'dotted' - dotted line
-            4: 'dashdot',           # 'dotdash' - dash-dot line
-            5: 'longdash',          # 'longdash' - long dashed line
-            6: 'longdashdot',       # 'twodash' - two dash line (not directly equivalent in Plotly, but 'longdashdot' is close)
-        }
+        if isinstance(color,int):
+            color = COL_MAPPING[color]
+        dash = kwargs.pop('lty', kwargs.pop('linetype', kwargs.pop('dash', None)))            
         if isinstance(dash,int):
-            dash = lty_mapping[dash]        
-        
+            dash = lty_mapping[dash]  
+        symbol = kwargs.pop('symbol',kwargs.pop('pch', kwargs.pop('shape', 'circle')))                  
         width = kwargs.pop('lwd', kwargs.pop('linewidth', kwargs.pop('width', None)))
         opacity = kwargs.pop('alpha', kwargs.pop('opacity', 1))
         kwargs['line'] = dict(color=color, width=width, dash=dash)
@@ -101,7 +87,6 @@ class line(go.Scatter):
         super().__init__(mode='lines', *args, **kwargs)
 
 #---#
-
 
 def litely(fig):
     plotly_fig = tls.mpl_to_plotly(fig)
